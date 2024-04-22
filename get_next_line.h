adb35297939dc0f8f73df42caa80d5c9a6103e91
Abbosjon Keldiyarov <abbosjon99@gmail.com>
2024-04-22 15:44:50 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: akeldiya <akeldiya@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 18:40:27 by akeldiya          #+#    #+#             */
-/*   Updated: 2024/04/02 01:11:00 by akeldiya         ###   ########.fr       */
+/*   Updated: 2024/04/22 15:35:34 by akeldiya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,20 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 2
+#  define BUFFER_SIZE 1
 # endif
 
+# include <stdint.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <stdint.h>
 
-typedef struct t_forjoin
-{
-	char	*nxtline;
-	size_t	i;
-}			t_fj;
-
-char		*get_next_line(int fd);
-t_fj		ft_strjoin(char const *s1, char const *s2);
-size_t		ft_strlen(const char *s);
-size_t		ft_strnlen(const char *s);
-char		*ft_strdup(const char *s);
-void	*ft_calloc(size_t nmemb, size_t size);
+char	*get_next_line(int fd);
+char	*ft_strdup(char const *s);
+char	*ft_strjoin(char **s1, char const *s2, int len2);
+char	*ft_charshifter(char *s1, int shift);
+char	*ft_free_error(char **s1, char *s2);
+int		ft_get_line(int fd, char **readed, int firstrun);
+int		ft_strnlen(char const *s);
+size_t	ft_strlen(char const *s);
 
 #endif
